a7b3589555b4ab1dbbca5175a189675688979ab6
MiguelBalbo <miguel.balbo@yahoo.com.br>
2025-11-12 12:00:59 -0300

    Instalação do TailwindCss

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.querySelector('#root')
+if (!root) throw new Error('Failed to find the root element')
+createRoot(root).render(<App />)
