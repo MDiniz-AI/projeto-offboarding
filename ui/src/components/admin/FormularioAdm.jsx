14c1b65be794384fb10712f340f1c37c57974eb3
MiguelBalbo <miguel.balbo@yahoo.com.br>
2025-11-27 14:08:59 -0300

    correção responsividade pág formulario adm

--- ui/src/components/admin/FormularioAdm.jsx
+++ ui/src/components/admin/FormularioAdm.jsx
@@ -30,27 +30,30 @@ export default () => {
         <div>
             <h1 className="text-primary font-title text-4xl text-center my-[2vh]">Formulário</h1>
             <div className="flex flex-row flex-wrap gap-[1vw]">    
-                <Squircle className="bg-secondary/30 md:w-[24vw] w-83 h-55 md:h-[27vh] px-[1.2vw] py-[1vh] flex-col" cornerRadius={20} cornerSmoothing={1}>
+                <Squircle className="bg-secondary/30 md:w-[24vw] w-83 h-55 md:h-56 px-[1.2vw] py-[1vh] flex-col" cornerRadius={20} cornerSmoothing={1}>
                     <h2 className="font-title text-primary text-2xl text-center mt-[1vw]">{objFormulario.nomeCategoria}</h2>
                     <div className="flex flex-row gap-[2vw]">
-                    <div className="relative">
-                        <progress class={`progress md:w-[10vw] w-39 h-[5vh] ${getBgClass(objFormulario.valorScore)}`}  value={objFormulario.valorScore * 100} max="100"></progress>
-                        
-                        <p className="text-primary text-center font-corpo text-md absolute bottom-7 md:bottom-8 left-15 md:left-[4vw]">
-                          {objFormulario.valorScore}
-                        </p>
-              
-                        <p className="text-primary text-center font-corpo text-xs">
-                          Score Médio
-                        </p>
-                    </div>
-              <div className="relative">
-                <progress class={`progress md:w-[10vw] w-39 h-[5vh] ${getBgClass(objFormulario.valorInten)}`} value={objFormulario.valorInten * 100} max="100"></progress>
-                
-                <p className="text-primary text-center font-corpo text-md absolute bottom-7 md:bottom-8 left-15 md:left-[4vw]">
-                    {objFormulario.valorInten}
-                  </p>
-                
+                      <div>
+                        <div className="relative">
+                            <progress class={`progress md:w-[10vw] w-39 h-[5vh] ${getBgClass(objFormulario.valorScore)}`}  value={objFormulario.valorScore * 100} max="100"></progress>
+                            
+                            <span className="absolute inset-0 flex items-center justify-center text-sm font-corpo text-primary">
+                              {objFormulario.valorScore}
+                            </span>
+                  
+                        </div>
+                          <p className="text-primary text-center font-corpo text-xs">
+                            Score Médio
+                          </p>
+                      </div>
+              <div>
+                <div className="relative">
+                  <progress class={`progress md:w-[10vw] w-39 h-[5vh] ${getBgClass(objFormulario.valorInten)}`} value={objFormulario.valorInten * 100} max="100"></progress>
+                  
+                  <span className="absolute inset-0 flex items-center justify-center text-sm font-corpo text-primary">
+                        {objFormulario.valorInten}
+                  </span>
+                </div>
                 <p className="text-primary text-center font-corpo text-xs">
                   Intensidade Média
                 </p>
@@ -60,7 +63,7 @@ export default () => {
             onClick={() => document.getElementById('modalEdicao').showModal()}
               cornerRadius={10}
               cornerSmoothing={1}
-              className="flex bg-secondary/50 w-40 h-[7vh] justify-center mt-[4vh] mx-auto"
+              className="flex bg-secondary/50 w-40 h-[7vh] justify-center mt-[2vh] mx-auto"
             >
               <DotsThreeIcon size="4vh" weight="thin" className="my-auto" />
               <p className="text-primary font-corpo my-auto">Detalhes</p>
@@ -80,29 +83,34 @@ export default () => {
             {objFormulario.nomeCategoria}
           </h1>
           <div className="flex flex-row gap-[2vw] mt-[1vh]">
-            <div className="relative flex-1">
-                <progress class={`progress md:w-full w-39 h-[5vh] ${getBgClass(objFormulario.valorScore)}`}  value={objFormulario.valorScore * 100} max="100"></progress>
-                
-                <p className="text-primary text-center font-corpo text-md absolute bottom-15 md:bottom-10 left-15 md:left-[20vw]">
-                  {objFormulario.valorScore}
-                </p>
-      
+            <div className="flex-1">
+              <div className="relative w-full">
+                  <progress class={`progress md:w-full w-39 h-[5vh] ${getBgClass(objFormulario.valorScore)}`}  value={objFormulario.valorScore * 100} max="100"></progress>
+                  
+                  <span className="absolute inset-0 flex items-center justify-center text-sm font-corpo text-primary">
+                        {objFormulario.valorScore}
+                  </span>
+        
+              </div>
                 <p className="text-primary text-center font-corpo text-md">
                   Score Médio
                 </p>
             </div>
 
-            <div className="relative flex-1">
-                <progress class={`progress md:w-full w-39 h-[5vh] ${getBgClass(objFormulario.valorInten)}`}  value={objFormulario.valorInten * 100} max="100"></progress>
-                
-                <p className="text-primary text-center font-corpo text-md absolute bottom-15 md:bottom-10 left-15 md:left-[20vw]">
-                  {objFormulario.valorInten}
-                </p>
-      
-                <p className="text-primary text-center font-corpo text-md">
-                  Intensidade Média
-                </p>
+            <div className="flex-1">
+              <div className="relative w-full">
+
+                  <progress class={`progress md:w-full w-39 h-[5vh] ${getBgClass(objFormulario.valorInten)}`}  value={objFormulario.valorInten * 100} max="100"></progress>
+                  
+                  <span className="absolute inset-0 flex items-center justify-center text-sm font-corpo text-primary">
+                        {objFormulario.valorInten}
+                  </span>
             </div>          
+        
+                  <p className="text-primary text-center font-corpo text-md">
+                    Intensidade Média
+                  </p>
+            </div>
           </div>
           <div className="mt-[2vh]">
               <div className="flex flex-row md:gap-[68vw] gap-15">
