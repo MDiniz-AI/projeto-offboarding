eec216b47bfc0e3618b62c252aed7973c15c0ac0
Joice Cristina da Silva Santos <joice.santos@blip.ai>
2025-11-27 23:45:38 -0300

    Terminando parte do formulario

--- ui/src/components/admin/FormularioAdm.jsx
+++ ui/src/components/admin/FormularioAdm.jsx
@@ -2,22 +2,52 @@ import { createContext, useContext, useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Squircle } from "corner-smoothing";
 import { CaretDownIcon, DotsThreeIcon, PlusIcon } from "@phosphor-icons/react";
-
+import api from "../../lib/api.js";
 import PerguntasEdit from "./PerguntasEdit";
 
 export default () => {
+ 
+  const [perguntas, setPerguntas] = useState([]);
+  const [perguntaAberta, setPerguntaAberta] = useState(null);
+  const [edicoes, setEdicoes] = useState({});
+  const [tipoCampo, setTipoCampo] = useState({});
+  const [opcoes, setOpcoes] = useState({});
 
-    const [perg, setPerg] = useState([""]);
+  function adicionarPerg() {
+    setPerg((prev) => [...prev, ""]); // adiciona mais um vazio
+  }
 
-    function adicionarPerg() {
-            setPerg((prev) => [...prev, ""]); // adiciona mais um vazio
+  useEffect(() => {
+    async function carregarPerguntas() {
+      try {
+        const response = await api.get("/perguntas/");
+        const todasPerguntas = response.data.flat();
+        setPerguntas(todasPerguntas);
+
+        const edicoesInit = {};
+        const tipoInit = {};
+        const opcoesInit = {};
+        todasPerguntas.forEach((p) => {
+          edicoesInit[p.id_pergunta] = p.texto_pergunta;
+          tipoInit[p.id_pergunta] = p.tipo || "";
+          opcoesInit[p.id_pergunta] = p.opcoes || [""];
+        });
+
+        setEdicoes(edicoesInit);
+        setTipoCampo(tipoInit);
+        setOpcoes(opcoesInit);
+      } catch (err) {
+        console.error("Erro ao carregar perguntas:", err);
+      }
     }
+    carregarPerguntas();
+  }, []);
 
-    const objFormulario = {
-        nomeCategoria: "Salário e Benefícios",
-        valorScore: 0.5,
-        valorInten: 0.75,
-    }
+  const objFormulario = {
+    nomeCategoria: "Salário e Benefícios",
+    valorScore: 0.5,
+    valorInten: 0.75,
+  };
 
   const getBgClass = (valor) => {
     if (valor >= 0.75) return "progress-success";
@@ -25,39 +55,59 @@ export default () => {
     return "progress-error";
   };
 
-
-    return (<div>
-        <div>
-            <h1 className="text-primary font-title text-4xl text-center my-[2vh]">Formulário</h1>
-            <div className="flex flex-row flex-wrap gap-[1vw]">    
-                <Squircle className="bg-secondary/30 md:w-[24vw] w-83 h-55 md:h-[27vh] px-[1.2vw] py-[1vh] flex-col" cornerRadius={20} cornerSmoothing={1}>
-                    <h2 className="font-title text-primary text-2xl text-center mt-[1vw]">{objFormulario.nomeCategoria}</h2>
-                    <div className="flex flex-row gap-[2vw]">
-                    <div className="relative">
-                        <progress class={`progress md:w-[10vw] w-39 h-[5vh] ${getBgClass(objFormulario.valorScore)}`}  value={objFormulario.valorScore * 100} max="100"></progress>
-                        
-                        <p className="text-primary text-center font-corpo text-md absolute bottom-7 md:bottom-8 left-15 md:left-[4vw]">
-                          {objFormulario.valorScore}
-                        </p>
-              
-                        <p className="text-primary text-center font-corpo text-xs">
-                          Score Médio
-                        </p>
-                    </div>
+  return (
+    <div>
+      <div>
+        <h1 className="text-primary font-title text-4xl text-center my-[2vh]">
+          Formulário
+        </h1>
+        <div className="flex flex-row flex-wrap gap-[1vw]">
+          <Squircle
+            className="bg-secondary/30 md:w-[24vw] w-83 h-55 md:h-[27vh] px-[1.2vw] py-[1vh] flex-col"
+            cornerRadius={20}
+            cornerSmoothing={1}
+          >
+            <h2 className="font-title text-primary text-2xl text-center mt-[1vw]">
+              {objFormulario.nomeCategoria}
+            </h2>
+            <div className="flex flex-row gap-[2vw]">
               <div className="relative">
-                <progress class={`progress md:w-[10vw] w-39 h-[5vh] ${getBgClass(objFormulario.valorInten)}`} value={objFormulario.valorInten * 100} max="100"></progress>
-                
+                <progress
+                  class={`progress md:w-[10vw] w-39 h-[5vh] ${getBgClass(
+                    objFormulario.valorScore
+                  )}`}
+                  value={objFormulario.valorScore * 100}
+                  max="100"
+                ></progress>
+
                 <p className="text-primary text-center font-corpo text-md absolute bottom-7 md:bottom-8 left-15 md:left-[4vw]">
-                    {objFormulario.valorInten}
-                  </p>
-                
+                  {objFormulario.valorScore}
+                </p>
+
+                <p className="text-primary text-center font-corpo text-xs">
+                  Score Médio
+                </p>
+              </div>
+              <div className="relative">
+                <progress
+                  class={`progress md:w-[10vw] w-39 h-[5vh] ${getBgClass(
+                    objFormulario.valorInten
+                  )}`}
+                  value={objFormulario.valorInten * 100}
+                  max="100"
+                ></progress>
+
+                <p className="text-primary text-center font-corpo text-md absolute bottom-7 md:bottom-8 left-15 md:left-[4vw]">
+                  {objFormulario.valorInten}
+                </p>
+
                 <p className="text-primary text-center font-corpo text-xs">
                   Intensidade Média
                 </p>
               </div>
             </div>
             <Squircle
-            onClick={() => document.getElementById('modalEdicao').showModal()}
+              onClick={() => document.getElementById("modalEdicao").showModal()}
               cornerRadius={10}
               cornerSmoothing={1}
               className="flex bg-secondary/50 w-40 h-[7vh] justify-center mt-[4vh] mx-auto"
@@ -81,44 +131,79 @@ export default () => {
           </h1>
           <div className="flex flex-row gap-[2vw] mt-[1vh]">
             <div className="relative flex-1">
-                <progress class={`progress md:w-full w-39 h-[5vh] ${getBgClass(objFormulario.valorScore)}`}  value={objFormulario.valorScore * 100} max="100"></progress>
-                
-                <p className="text-primary text-center font-corpo text-md absolute bottom-15 md:bottom-10 left-15 md:left-[20vw]">
-                  {objFormulario.valorScore}
-                </p>
-      
-                <p className="text-primary text-center font-corpo text-md">
-                  Score Médio
-                </p>
+              <progress
+                class={`progress md:w-full w-39 h-[5vh] ${getBgClass(
+                  objFormulario.valorScore
+                )}`}
+                value={objFormulario.valorScore * 100}
+                max="100"
+              ></progress>
+
+              <p className="text-primary text-center font-corpo text-md absolute bottom-15 md:bottom-10 left-15 md:left-[20vw]">
+                {objFormulario.valorScore}
+              </p>
+
+              <p className="text-primary text-center font-corpo text-md">
+                Score Médio
+              </p>
             </div>
 
             <div className="relative flex-1">
-                <progress class={`progress md:w-full w-39 h-[5vh] ${getBgClass(objFormulario.valorInten)}`}  value={objFormulario.valorInten * 100} max="100"></progress>
-                
-                <p className="text-primary text-center font-corpo text-md absolute bottom-15 md:bottom-10 left-15 md:left-[20vw]">
-                  {objFormulario.valorInten}
-                </p>
-      
-                <p className="text-primary text-center font-corpo text-md">
-                  Intensidade Média
-                </p>
-            </div>          
+              <progress
+                class={`progress md:w-full w-39 h-[5vh] ${getBgClass(
+                  objFormulario.valorInten
+                )}`}
+                value={objFormulario.valorInten * 100}
+                max="100"
+              ></progress>
+
+              <p className="text-primary text-center font-corpo text-md absolute bottom-15 md:bottom-10 left-15 md:left-[20vw]">
+                {objFormulario.valorInten}
+              </p>
+
+              <p className="text-primary text-center font-corpo text-md">
+                Intensidade Média
+              </p>
+            </div>
           </div>
           <div className="mt-[2vh]">
-              <div className="flex flex-row md:gap-[68vw] gap-15">
-                  <h2 className="font-title md:text-[1.7vw] text-[6vw] text-primary mt-5">Perguntas</h2>
-                  <Squircle cornerRadius={10} cornerSmoothing={1} className="flex bg-secondary/50 md:w-[10vw] w-33 h-[7vh] justify-center mt-[1vh]" onClick={adicionarPerg}>
-                      <PlusIcon size="4vh" weight="thin" className="my-auto" />
-                      <p className="text-primary font-corpo my-auto">Adicionar</p>
-                  </Squircle>
-              </div>
-              <div>
-                  {perg.map((valor, index) => (
-                      <PerguntasEdit />
-                  ))}
-              </div>
-                </div>
+            <div className="flex flex-row md:gap-[68vw] gap-15">
+              <h2 className="font-title md:text-[1.7vw] text-[6vw] text-primary mt-5">
+                Perguntas
+              </h2>
+              <Squircle
+                cornerRadius={10}
+                cornerSmoothing={1}
+                className="flex bg-secondary/50 md:w-[10vw] w-33 h-[7vh] justify-center mt-[1vh]"
+                onClick={adicionarPerg}
+              >
+                <PlusIcon size="4vh" weight="thin" className="my-auto" />
+                <p className="text-primary font-corpo my-auto">Adicionar</p>
+              </Squircle>
             </div>
-        </dialog>
-    </div>)
-}
+            <div>
+              {perguntas.map((p) => (
+                <PerguntasEdit
+                  key={p.id_pergunta}
+                  pergunta={p}
+                  aberto={perguntaAberta === p.id_pergunta} 
+                  onToggle={() =>
+                    setPerguntaAberta((prev) =>
+                      prev === p.id_pergunta ? null : p.id_pergunta
+                    )
+                  }
+                  edicao={edicoes[p.id_pergunta]}
+                  tipo={tipoCampo[p.id_pergunta]}
+                  opcoes={opcoes[p.id_pergunta]}
+                  setEdicoes={setEdicoes}
+                  setTipoCampo={setTipoCampo}
+                  setOpcoes={setOpcoes}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+};
--- ui/src/components/admin/PerguntasEdit.jsx
+++ ui/src/components/admin/PerguntasEdit.jsx
@@ -1,73 +1,246 @@
-import InputCurto from "../InputCurto.jsx"
-import InputLongo from "../InputLongo.jsx"
-import Seletor from "../Seletor.jsx"
-import { CaretDownIcon, FloppyDiskIcon, PlusIcon, TrashIcon } from "@phosphor-icons/react";
-import { createContext, useContext, useState, useEffect } from 'react'
-import { Squircle } from "corner-smoothing"; 
+import InputCurto from "../InputCurto.jsx";
+import InputLongo from "../InputLongo.jsx";
+import Seletor from "../Seletor.jsx";
+import {
+  CaretDownIcon,
+  FloppyDiskIcon,
+  PlusIcon,
+  TrashIcon,
+} from "@phosphor-icons/react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { Squircle } from "corner-smoothing";
 import api from "../../lib/api.js";
 
-export default (props) => {
-    const [tipoCampo, setTipoCampo] = useState("");
-    const [opcoes, setOpcoes] = useState([""]);
-    const [aberto, setAberto] = useState(false);
+export default ({
+  pergunta,
+  edicao,
+  tipo,
+  opcoes: opcoesProps,
+  setEdicoes,
+  setTipoCampo,
+  onToggle,
+  setOpcoes,
+}) => {
+  const [tipoCampo, setTipoCampoLocal] = useState(tipo || "");
+  const [opcoes, setOpcoesLocal] = useState(opcoesProps || [""]);
+  const [aberto, setAberto] = useState(false);
+  const [textoPergunta, setTextoPergunta] = useState("");
+  //  const [idEmEdicao, setIdEmEdicao] = useState(null);
 
-    function adicionarOpcao() {
-            setOpcoes((prev) => [...prev, ""]); // adiciona mais um vazio
+  useEffect(() => {
+    if (aberto) {
+      setTextoPergunta(edicao);
+      setTipoCampoLocal(tipo);
+      setOpcoesLocal(opcoesProps);
     }
-
-    return <div>
-            <Squircle cornerRadius={10} cornerSmoothing={1} className="flex bg-secondary/50 w-auto h-[10vh] mt-[1vh] px-[2vw]">
-                <p className="text-primary font-corpo my-auto w-[82vw]">Pergunta 1 gdfhjkldfkg hhjklçdfglhkdfghkldfghklhkdfghkldfghlkhkldfghkldfghksgkldjkglkdfhklhglfkdhjgkldf hgjlj dfsgçolçdfghldf hkglç j?????</p>
-                <CaretDownIcon size="4vh" weight="thin" className="my-auto" onClick={() => setAberto((prev) => !prev)} />
-            </Squircle>
+  }, [aberto, edicao, tipo, opcoesProps]);
 
 
-            {aberto && <Squircle cornerRadius={10} cornerSmoothing={1} className="bg-secondary/10 w-auto h-auto px-[2vw] py-[2vh] ">
-                <div className="flex gap-[2vw] w-full">
-                    <div className="flex flex-col gap-[1vh] w-[65vw]">
-                        <label for={1} className="font-corpo md:text-[1vw] md:ml-0 ml-[2vw] text-[4vw] text-primary">Pergunta</label>
-                        <input name={1} type="text" id={1} placeholder="Digite pergunta aqui" value="Pergunta" className="bg-secondary/30 p-[2vh] md:w-full md:mx-0 w-full mx-auto font-corpo rounded-xl md:text-[1vw] text-[4vw] text-primary"/>
-                    </div>
+  const toggleAberto = () => {
+    setAberto((prev) => !prev);
+  };
 
-                    <div className="flex flex-col w-[30vw]">
-                        <label for={2} className="font-corpo md:text-[1vw] text-[4vw] text-primary text-justify">Tipo de resposta</label>
-                        <select name={2} id={2} className="bg-secondary/30 p-[2vh] w-full font-corpo rounded-xl md:text-[1vw] text-[4vw] text-primary mt-[1vh]" onChange={(e) => setTipoCampo(e.target.value)}>
-                            <option key={0} value="Selecione uma opção" disabled hidden selected="selected">Selecione uma opção</option>
-                            <option key={1} value="Texto Curto" >Texto Curto</option>
-                            <option key={2} value="Texto Longo">Texto Longo</option>
-                            <option key={3} value="Lista de opções" >Lista de opções</option>
-                            <option key={4} value="Seletor de emoções" >Seletor de emoções</option>   
-                        </select>
-                    </div>
-                </div>
-                <div>
-                    {tipoCampo === "Lista de opções" && (
-                        <div className="flex flex-row gap-[1vw] flex-wrap">
-                            {opcoes.map((valor, index) => (
-                                <div key={index} className="flex flex-col gap-[1vh] mt-[2vh] w-[25vw]">
-                                    <label for={index} className="font-corpo md:text-[1vw] md:ml-0 ml-[2vw] text-[4vw] text-primary">{`Opção ${index + 1}`}</label>
-                                    <input name={index} type="text" id={index} placeholder={`Opção ${index + 1}`} className="bg-secondary/30 p-[2vh] md:w-full md:mx-0 w-full mx-auto font-corpo rounded-xl md:text-[1vw] text-[4vw] text-primary"/>
-                                </div>
-                            ))}
-                            
-                            <Squircle onClick={adicionarOpcao} cornerRadius={10} cornerSmoothing={1} className="flex bg-secondary/50 w-[4vw] h-[7vh] justify-center mt-[6vh]">
-                                <PlusIcon size="4vh" weight="thin" className="my-auto" />
-                            </Squircle>
-                        </div>
-                    )}
-                </div>
-                <div className="flex gap-[1vw] mx-auto justify-center">
-                    <Squircle cornerRadius={10} cornerSmoothing={1} className="flex bg-secondary/50 w-[10vw] h-[7vh] justify-center mt-[2vh]">
-                        <FloppyDiskIcon size="4vh" weight="thin" className="my-auto" />
-                        <p className="text-primary font-corpo my-auto">Salvar</p>
-                    </Squircle>
-                    <Squircle cornerRadius={10} cornerSmoothing={1} className="flex bg-secondary/50 w-[10vw] h-[7vh] justify-center mt-[2vh]">
-                        <TrashIcon size="4vh" weight="thin" className="my-auto" />
-                        <p className="text-primary font-corpo my-auto">Apagar</p>
-                    </Squircle>
-                </div>
-                
+  const handleEdicao = (valor) => {
+    setTextoPergunta(valor);
+    setEdicoes((prev) => ({ ...prev, [pergunta.id_pergunta]: valor }));
+  };
 
-            </Squircle>}
-        </div>
+  // const handleTipo = (valor) => {
+  //   setTipoCampoLocal(valor);
+  //   setTipoCampo((prev) => ({ ...prev, [pergunta.id]: valor }));
+  // };
+
+  const handleOpcao = (index, valor) => {
+    const novasOpcoes = [...opcoes];
+    novasOpcoes[index] = valor;
+    setOpcoesLocal(novasOpcoes);
+    setOpcoes((prev) => ({ ...prev, [pergunta.id]: novasOpcoes }));
+  };
+
+ function mapTipoResposta(texto) {
+  switch (texto) {
+    case "Texto": return 0;
+    case "Texto longo": return 1;
+    case "Select de opções": return 2;
+    case "Seletor de emoções": return 3;
+    default: return 0;
+  }
 }
+
+
+  const salvarEdicao = async () => {
+    try {
+     const body = {
+      texto_pergunta: textoPergunta,
+      tipo_resposta: mapTipoResposta(tipoCampo), 
+      opcoes: opcoes
+    };
+
+      const response = await api.put(`/perguntas/${pergunta.id_pergunta}`, body);
+        console.log("📤 Payload enviado para o backend:", body);
+    console.log("📌 ID da pergunta sendo atualizada:", pergunta.id_pergunta);
+
+
+      console.log("Pergunta atualizada:", response.data);
+
+
+      toggleAberto();
+      //onToggle();
+    } catch (err) {
+      console.error("Erro ao atualizar pergunta:", err);
+    }
+  };
+
+  function adicionarOpcao() {
+    setOpcoes((prev) => [...prev, ""]); // adiciona mais um vazio
+  }
+
+  return (
+    <div>
+      <Squircle
+        cornerRadius={10}
+        cornerSmoothing={1}
+        className="flex bg-secondary/50 w-auto h-[10vh] mt-[1vh] px-[2vw]"
+      >
+        <p className="text-primary font-corpo my-auto w-[82vw]">
+          {" "}
+          {pergunta.texto_pergunta}{" "}
+        </p>
+        <CaretDownIcon
+          size="4vh"
+          weight="thin"
+          className="my-auto"
+         onClick={toggleAberto}
+        />
+      </Squircle>
+
+      {aberto && (
+        <Squircle
+          cornerRadius={10}
+          cornerSmoothing={1}
+          className="bg-secondary/10 w-auto h-auto px-[2vw] py-[2vh] "
+        >
+          <div className="flex gap-[2vw] w-full">
+            <div className="flex flex-col gap-[1vh] w-[65vw]">
+              <label
+                for={1}
+                className="font-corpo md:text-[1vw] md:ml-0 ml-[2vw] text-[4vw] text-primary"
+              >
+                Pergunta
+              </label>
+              <input
+                name="textoPergunta"
+                type="text"
+                id={1}
+                placeholder="Digite pergunta aqui"
+                onChange={(e) => setTextoPergunta(e.target.value)}
+                value={textoPergunta}
+                className="bg-secondary/30 p-[2vh] md:w-full md:mx-0 w-full mx-auto font-corpo rounded-xl md:text-[1vw] text-[4vw] text-primary"
+              />
+            </div>
+
+            <div className="flex flex-col w-[30vw]">
+              <label
+                for={2}
+                className="font-corpo md:text-[1vw] text-[4vw] text-primary text-justify"
+              >
+                Tipo de resposta
+              </label>
+              <select
+                name="tipoCampo"
+                id={2}
+                value={tipoCampo[pergunta.id_pergunta]}
+                className="bg-secondary/30 p-[2vh] w-full font-corpo rounded-xl md:text-[1vw] text-[4vw] text-primary mt-[1vh]"
+                 onChange={(e) =>
+                setTipoCampo((prev) => ({
+                  ...prev,
+                  [pergunta.id_pergunta]: e.target.value,
+                }))
+              }
+              >
+                <option
+                  key={0}
+                  value="Selecione uma opção"
+                  disabled
+                  hidden
+                  selected="selected"
+                >
+                  Selecione uma opção
+                </option>
+                <option key={1} value="Texto Curto">
+                  Texto Curto
+                </option>
+                <option key={2} value="Texto Longo">
+                  Texto Longo
+                </option>
+                <option key={3} value="Lista de opções">
+                  Lista de opções
+                </option>
+                <option key={4} value="Seletor de emoções">
+                  Seletor de emoções
+                </option>
+              </select>
+            </div>
+          </div>
+          <div>
+            {tipoCampo === "Lista de opções" && (
+              <div className="flex flex-row gap-[1vw] flex-wrap">
+                {opcoes.map((valor, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col gap-[1vh] mt-[2vh] w-[25vw]"
+                  >
+                    <label
+                      for={index}
+                      className="font-corpo md:text-[1vw] md:ml-0 ml-[2vw] text-[4vw] text-primary"
+                    >{`Opção ${index + 1}`}</label>
+                    <input
+                      name="opcoes"
+                      type="text"
+                      id={index}
+                      value={valor[pergunta.id_pergunta]}
+                      placeholder={`Opção ${index + 1}`}
+                      className="bg-secondary/30 p-[2vh] md:w-full md:mx-0 w-full mx-auto font-corpo rounded-xl md:text-[1vw] text-[4vw] text-primary"
+                    />
+                  </div>
+                ))}
+
+                <Squircle
+                  onClick={adicionarOpcao}
+                  cornerRadius={10}
+                  cornerSmoothing={1}
+                  className="flex bg-secondary/50 w-[4vw] h-[7vh] justify-center mt-[6vh]"
+                >
+                  <PlusIcon size="4vh" weight="thin" className="my-auto" />
+                </Squircle>
+              </div>
+            )}
+          </div>
+          <div className="flex gap-[1vw] mx-auto justify-center">
+            <Squircle
+              cornerRadius={10}
+              cornerSmoothing={1}
+              className="flex bg-secondary/50 w-[10vw] h-[7vh] justify-center mt-[2vh]"
+            >
+              <FloppyDiskIcon size="4vh" weight="thin" className="my-auto" />
+              <button
+                onClick={salvarEdicao}
+                className="text-primary font-corpo my-auto"
+              >
+                Salvar edição
+              </button>
+            </Squircle>
+            <Squircle
+              cornerRadius={10}
+              cornerSmoothing={1}
+              className="flex bg-secondary/50 w-[10vw] h-[7vh] justify-center mt-[2vh]"
+            >
+              <TrashIcon size="4vh" weight="thin" className="my-auto" />
+              <p className="text-primary font-corpo my-auto">Apagar</p>
+            </Squircle>
+          </div>
+        </Squircle>
+      )}
+    </div>
+  );
+};
